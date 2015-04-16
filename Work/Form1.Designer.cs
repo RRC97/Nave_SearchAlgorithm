4739e7307debec9c5aa6ca3452b3201842ad6123
RRC97 <matheuspereira.nave@gmail.com>
2015-04-15 21:54:31 -0300

    Version 15/04

---
+++ Work/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Work
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonFind = new System.Windows.Forms.Button();
+            this.listBoxElements = new System.Windows.Forms.ListBox();
+            this.textBoxFind = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonFind
+            // 
+            this.buttonFind.Location = new System.Drawing.Point(138, 38);
+            this.buttonFind.Name = "buttonFind";
+            this.buttonFind.Size = new System.Drawing.Size(98, 23);
+            this.buttonFind.TabIndex = 0;
+            this.buttonFind.Text = "Buscar";
+            this.buttonFind.UseVisualStyleBackColor = true;
+            this.buttonFind.Click += new System.EventHandler(this.buttonFind_Click);
+            // 
+            // listBoxElements
+            // 
+            this.listBoxElements.FormattingEnabled = true;
+            this.listBoxElements.Location = new System.Drawing.Point(12, 12);
+            this.listBoxElements.Name = "listBoxElements";
+            this.listBoxElements.Size = new System.Drawing.Size(120, 225);
+            this.listBoxElements.TabIndex = 1;
+            // 
+            // textBoxFind
+            // 
+            this.textBoxFind.Location = new System.Drawing.Point(138, 12);
+            this.textBoxFind.Name = "textBoxFind";
+            this.textBoxFind.Size = new System.Drawing.Size(100, 20);
+            this.textBoxFind.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(248, 249);
+            this.Controls.Add(this.textBoxFind);
+            this.Controls.Add(this.listBoxElements);
+            this.Controls.Add(this.buttonFind);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonFind;
+        private System.Windows.Forms.ListBox listBoxElements;
+        private System.Windows.Forms.TextBox textBoxFind;
+    }
+}
+
---
+++ Work/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Work
+{
+    public partial class Form1 : Form
+    {
+        private int total;
+        private int[] elements;
+        public Form1()
+        {
+            InitializeComponent();
+
+            total = 1000;
+
+            elements = CreateElements();
+            SelectionSort(ref elements);
+            SetOnListBox();
+        }
+
+        int[] CreateElements()
+        {
+            Random rand = new Random();
+            List<int> listElement = new List<int>();
+            for (int i = 0; i < total; i++)
+            {
+                listElement.Add(rand.Next(total * 2));
+            }
+
+            return listElement.ToArray();
+        }
+
+        void SelectionSort(ref int[] array)
+        {
+            int temp;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+        }
+
+        void SetOnListBox()
+        {
+            foreach(int element in elements)
+            {
+                listBoxElements.Items.Add(element);
+            }
+        }
+
+        void BinarySearch(int[] array, int value)
+        {
+            int  pointer = array.Length / 2;
+            while (array[pointer] != value)
+                if (array[pointer] > value)
+                    pointer--;
+                else
+                    pointer++;
+            MessageBox.Show("A posição do elemento é em " + (pointer + 1) + "º lugar", "Resultado");
+        }
+
+        private void buttonFind_Click(object sender, EventArgs e)
+        {
+            int value = -1;
+            if (int.TryParse(textBoxFind.Text, out value))
+                if (elements.Contains<int>(value))
+                    BinarySearch(elements, value);
+                else 
+                    MessageBox.Show("Este número não contém na lista", "Erro");
+            else 
+                MessageBox.Show("Por favor, digite somente números", "Erro");
+        }
+    }
+}
